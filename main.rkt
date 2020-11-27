fc5e1a3b7db602b4797717ed85259ca9d226162e
Sam Phillips <samdphillips@gmail.com>
2020-11-27 11:09:41 -0800

    Use GLib style naming convention.

--- main.rkt
+++ main.rkt
@@ -1,36 +1,36 @@
 #lang racket/base
 
 (require ffi/unsafe
-         ffi/unsafe/define
-         ffi/unsafe/define/conventions)
+         ffi/unsafe/define)
 
 (define gst-lib
   (ffi-lib "libgstreamer-1.0"))
 
-(define-ffi-definer define-gst gst-lib
-  #:make-c-id convention:hyphen->underscore)
+(define-ffi-definer define-gst gst-lib)
 
 (define _gboolean _bool)
-(define _gst-clock-time _uint64)
+(define _GstClockTime _uint64)
 
-(define-cpointer-type _gst-element)
-(define-cpointer-type _gst-bus)
-(define-cpointer-type _gst-message)
+(define-cpointer-type _GstObject)
+(define-cpointer-type _GstElement)
+(define-cpointer-type _GstBus)
+(define-cpointer-type _GstMessage)
+(define-cpointer-type _GstMiniObject)
 
-(define _gst-state
+(define _GstState
   (_enum '[GST_STATE_VOID_PENDING = 0
            GST_STATE_NULL         = 1
            GST_STATE_READY        = 2
            GST_STATE_PAUSED       = 3
            GST_STATE_PLAYING      = 4]))
 
-(define _gst-state-change-return
+(define _GstStateChangeReturn
   (_enum '[GST_STATE_CHANGE_FAILURE    = 0
            GST_STATE_CHANGE_SUCCESS    = 1
            GST_STATE_CHANGE_ASYNC      = 2
            GST_STATE_CHANGE_NO_PREROLL = 3]))
 
-(define _gst-message-type
+(define _GstMessageType
   (_bitmask '[GST_MESSAGE_UNKNOWN = 0
               GST_MESSAGE_EOS     = 1
               GST_MESSAGE_ERROR   = 2]))
@@ -38,48 +38,57 @@
 (define GST_CLOCK_TIME_NONE #xFFFFFFFFFFFFFFFF)
 
 ;; FIXME: don't pass in nulls
-(define-gst gst-init-check
+(define-gst gst_init_check
   (_fun (_pointer = #f)
         (_pointer = #f)
         (_pointer = #f)
         -> _gboolean))
 
 ;; FIXME: don't pass in null for error
-(define-gst gst-parse-launch
+(define-gst gst_parse_launch
   (_fun _string/utf-8
         (_pointer = #f)
         ->
-        _gst-element/null))
+        _GstElement/null))
 
-(define-gst gst-element-set-state
-  (_fun _gst-element _gst-state
+(define-gst gst_object_unref
+  (_fun _GstObject -> _void))
+
+(define-gst gst_element_set_state
+  (_fun _GstElement _GstState
         ->
-        _gst-state-change-return))
+        _GstStateChangeReturn))
 
-(define-gst gst-element-get-bus
-  (_fun _gst-element -> _gst-bus/null))
+(define-gst gst_element_get_bus
+  (_fun _GstElement -> _GstBus/null))
 
-(define-gst gst-bus-timed-pop-filtered
-  (_fun _gst-bus
-        _gst-clock-time
-        _gst-message-type
+(define-gst gst_bus_timed_pop_filtered
+  (_fun _GstBus
+        _GstClockTime
+        _GstMessageType
         ->
-        _gst-message/null))
+        _GstMessage/null))
+
+(define-gst gst_mini_object_unref
+  (_fun _GstMiniObject -> _void))
+
+(define (gst_message_unref gst-message)
+  (gst_mini_object_unref
+    (cast gst-message _GstMessage _GstMiniObject)))
 
 (module* tutorial-1 #f
-  (gst-init-check)
+  (gst_init_check)
   (define pipeline
-    (gst-parse-launch
+    (gst_parse_launch
       "playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm"))
-  (gst-element-set-state pipeline 'GST_STATE_PLAYING)
-  (define bus (gst-element-get-bus pipeline))
+  (gst_element_set_state pipeline 'GST_STATE_PLAYING)
+  (define bus (gst_element_get_bus pipeline))
   (define msg
-    (gst-bus-timed-pop-filtered
+    (gst_bus_timed_pop_filtered
       bus GST_CLOCK_TIME_NONE '(GST_MESSAGE_ERROR GST_MESSAGE_EOS)))
-
   (when msg
-    (gst-message-unref msg))
-  (gst-object-unref bus)
-  (gst-element-set-state pipeline 'GST_STATE_NULL)
-  (gst-object-unref pipeline))
+    (gst_message_unref msg))
+  (gst_object_unref (cast bus _GstBus _GstObject))
+  (gst_element_set_state pipeline 'GST_STATE_NULL)
+  (gst_object_unref (cast pipeline _GstElement _GstObject)))
 
