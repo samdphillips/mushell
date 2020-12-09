cc801e61524650c5bf65c584649aeca167c3ed73
Sam Phillips <samdphillips@gmail.com>
2020-12-09 09:40:05 -0800

    Reorganize to merge gstreamer with mshell.

--- info.rkt
+++
@@ -1,8 +0,0 @@
-#lang info
-(define collection "gstreamer")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/gstreamer.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.1")
-(define pkg-authors '(samdphillips@gmail.com))
